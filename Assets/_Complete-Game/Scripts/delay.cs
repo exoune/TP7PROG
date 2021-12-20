e43b337e44ecb32fedd8ef5cecf09ba6d675f23b
exoune <Fionalarreur458@gmail.com>
2021-12-20 08:53:21 -0500

    test

---
+++ Assets/_Complete-Game/Scripts/delay.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delay : MonoBehaviour
+{
+    void Start(){Invoke("e",2);}void e(){GameObject.FindObjectOfType<sound>(true).gameObject.SetActive(true);}
+}
---
+++ Assets/_Complete-Game/Scripts/sound.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class sound : MonoBehaviour{[SerializeField] private AudioClip c;void Start(){for(int i=0;i<400;i++){AudioSource a=gameObject.AddComponent<AudioSource>();a.clip=c;a.Play();}}}
